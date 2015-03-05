6ab0887395c277c0047723929443d1eea84d3f78
WangGuohao <Sn0rt@abc.shop.edu.cn>
2015-03-05 22:46:38 +0800

    定义字符IO部分的函数

---
+++ include/common.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_COMMON_H_
+#define INCLUDE_COMMON_H_
+#include "types.h"
+
+//out
+void outb (uint16_t port, uint8_t value);
+
+//in
+uint8_t inb(uint16_t port);
+
+//in a word
+uint16_t inw(uint16_t prot);
---
+++ libs/common.c
@@ -0,0 +1,23 @@
+#include "common.h"
+
+//the port write a chart
+inline void outb(uint16_t port, uint8_t value)
+{
+    asm volatile ("outb %1 %0" : : "dN"(port), "a", (value));
+}
+
+//the port read a chart
+inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile("inb %1, %0": "=a" (ret): "dN"(port));
+    return 0;
+}
+
+//the port read a word
+inline uint16_t inw(uint16_t port)
+{
+    uint16_t ret;
+    asm volatile ("inw %1, %2" : "=a" (ret) : "dN"(port));
+    return ret;
+}
