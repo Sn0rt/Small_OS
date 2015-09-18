23c1d19b041db25356c5e918a7e41f7e587812fb
WangGuohao <Sn0rt@abc.shop.edu.cn>
2015-09-18 12:47:55 +0800

    准备字符驱动

---
+++ include/console.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_CONSOLE_H_
+#define INCLUDE_CONSOLE_H_
+
+#include "common.h"
+
+//The constat of color
+typedef enum real_color {
+    rc_black = 0,
+    rc_bule = 1,
+    rc_gree =  2,
+    rc_cyan = 4,
+}
